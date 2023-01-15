cec756768bd96d6376e1d321d7d6446b1aae2656
rsveinson <rsveinson@mts.net>
2023-01-15 12:21:55 -0600

    added FormatTemplateV3 includes String[] tokens and String delim used for complext mixed type input records

---
+++ FormatTemplateV3.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+import javax.swing.*;
+import java.io.*;
+import java.text.NumberFormat;
+
+/** ***************************************************
+ *  Name:           your name
+ *  Class:          CS20S
+ * 
+ *  Assignment:     Ax Qy
+ * 
+ *************************************************************/
+
+public class FormatTemplateV3 {
+
+    public static void main(String[] args) throws IOException{
+    // ***** constants *******
+    
+    // ***** variables *****
+    
+        String banner = "";             // output banner
+        String prompt = "";             // prompt for user input
+        
+        String strin = "";              // string fro keyboard input
+        String strout = "";             // string for formatted output
+        
+        String delim = "[ ]+";          // delimiter for splitting input records
+        String[] tokens = null;         // used to split input records
+        
+        // a new line character that works on every computer system
+        String nl = System.lineSeparator();
+    
+    // ***** objects *****
+    
+        //Scanner scanner = new Scanner(System.in);
+        //NumberFormat currency = NumberFormat.getCurrencyInstance();
+    
+    // ***** print banners *****
+    
+        banner = "*****************************" + nl;
+        banner += "Name:        Sveinson" + nl;
+        banner += "Class:       CS30S" + nl;
+        banner += "Assignment:  Ax Qy" + nl;
+        banner += "*****************************" + nl + nl;
+        
+        System.out.println(banner);
+        //fout.print(banner);
+    
+    // ***** Get Input *****
+    
+        // prompt for input
+        // read input from keyboard
+        // echo input back to console window
+    
+    // ***** Main Processing *****
+    
+    // ***** Print Formatted Output *****
+    
+    // ***** Closing Message *****
+    
+        System.out.println();
+        System.out.println("end of processing");
+        //fout.println("End of Processing");
+        
+    } // end main 
+    
+} // end FormatTemplate
