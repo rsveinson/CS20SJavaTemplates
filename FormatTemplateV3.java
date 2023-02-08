dffa416ab51f41b83e8a5a21fc12d02e41342ab0
rsveinson <rsveinson@mts.net>
2023-02-08 07:43:38 -0600

    updated template 3 with a new line constant

--- FormatTemplateV3.java
+++ FormatTemplateV3.java
@@ -14,54 +14,51 @@ import java.text.NumberFormat;
 public class FormatTemplateV3 {
 
     public static void main(String[] args) throws IOException{
-    // ***** constants *******
-    
-    // ***** variables *****
-    
+        // ***** constants *******
+        // a new line character that works on every computer system
+        final String nl = System.lineSeparator();
+        // ***** variables *****
+
         String banner = "";             // output banner
         String prompt = "";             // prompt for user input
-        
+
         String strin = "";              // string fro keyboard input
         String strout = "";             // string for formatted output
-        
+
         String delim = "[ ]+";          // delimiter for splitting input records
         String[] tokens = null;         // used to split input records
-        
-        // a new line character that works on every computer system
-        String nl = System.lineSeparator();
-    
-    // ***** objects *****
-    
+
+        // ***** objects *****
         //Scanner scanner = new Scanner(System.in);
         //NumberFormat currency = NumberFormat.getCurrencyInstance();
-    
-    // ***** print banners *****
-    
+
+        // ***** print banners *****
+
         banner = "*****************************" + nl;
         banner += "Name:        Sveinson" + nl;
         banner += "Class:       CS30S" + nl;
         banner += "Assignment:  Ax Qy" + nl;
         banner += "*****************************" + nl + nl;
-        
+
         System.out.println(banner);
         //fout.print(banner);
-    
-    // ***** Get Input *****
-    
+
+        // ***** Get Input *****
+
         // prompt for input
         // read input from keyboard
         // echo input back to console window
-    
-    // ***** Main Processing *****
-    
-    // ***** Print Formatted Output *****
-    
-    // ***** Closing Message *****
-    
+
+        // ***** Main Processing *****
+
+        // ***** Print Formatted Output *****
+
+        // ***** Closing Message *****
+
         System.out.println();
         System.out.println("end of processing");
         //fout.println("End of Processing");
-        
+
     } // end main 
-    
+
 } // end FormatTemplate
